4cdcb53308b91cb949001bc5b31a5fffae98a0c8
2bebetter <2bebetter@348296974@qq.com>
2020-07-04 23:35:50 +0800

    合并urls

--- django/safego/routes/urls.py
+++ django/safego/routes/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.conf.urls import include
+from django.urls import path,re_path,include
 from django.urls import path
-from routes import views
+from routes import views, search
 
 urlpatterns = [
-    path('riskIndex/', views.risk_index().post)
+    path('riskIndex/', views.risk_index().post),
+    path('search-form/', search.search_form),
+    re_path(r"^search_advise/$", search.search_advise, name="search_advise"),
+    re_path(r"^search_risk/$", search.search_risk, name="search_risk"),
 ]
