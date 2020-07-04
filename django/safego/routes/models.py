ae9032288ae5e62d7778badfa8d95ef92ad240e2
2bebetter <2bebetter@348296974@qq.com>
2020-07-04 23:40:24 +0800

    在model中添加test

--- django/safego/routes/models.py
+++ django/safego/routes/models.py
@@ -6,3 +6,6 @@ class Question(models.Model):
     address =models.TextField(blank=False, null=False)
     city =models.TextField(blank=True, null=True)
     body = models.TextField(blank=False, null=False)
+
+class Test(models.Model):
+    name=models.CharField(max_length=20)
